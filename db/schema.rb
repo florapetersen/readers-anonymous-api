890dad144ee1393ea1e4598b746aebbd3647dc0a
florapetersen <floramarina@comcast.net>
2021-03-10 08:48:21 -0800

    add book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_164452) do
+ActiveRecord::Schema.define(version: 2021_03_10_164806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_03_10_164452) do
     t.string "name", default: "", null: false
     t.string "category", default: "", null: false
     t.datetime "meeting_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
